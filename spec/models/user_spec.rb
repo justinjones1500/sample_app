5a21c9a42ff9c0e565bd709c38a34f38b625c4cd
Justin Jones <justinjones1500@yahoo.com>
2013-07-01 21:40:21 -0400

    Adds unique user email for signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -58,4 +58,13 @@ describe "when email format is invalid" do
       end      
     end
   end
+ describe "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
+  end
 end
