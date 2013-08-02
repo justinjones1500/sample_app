dbcdee14fd1c822a55b05e521d62c3e7b3ab5c19
Justin Jones <justinjones1500@yahoo.com>
2013-08-01 21:46:59 -0400

    Removed the gravatar change

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,6 +29,7 @@ describe "signup" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+	
     end
 
     describe "with valid information" do
