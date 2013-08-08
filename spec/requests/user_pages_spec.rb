6959c6721310b35454720fc50dda55962389cb2e
Justin Jones <justinjones1500@yahoo.com>
2013-08-08 13:05:06 -0400

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,7 +8,8 @@ describe "profile page" do
   before { visit user_path(user) }
 
   it { should have_content(user.name) }
-  it { should have_title(user.name) }
+  #it { should have_title(user.name) } #this does not work, use below
+  it { should have_selector('title', text: full_title(user.name)) }
 
   
 end
@@ -42,6 +43,16 @@ describe "signup" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        #let(:user) { User.find_by(email: 'user@example.com') }
+        let(:user) { User.find_by_email('user@example.com') }
+        it { should have_link('Sign out') }
+       # it { should have_title(user.name) }
+       it { should have_selector('title', text: full_title(user.name)) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
