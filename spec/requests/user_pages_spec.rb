25ba4078d247ab6dae0b4c5752729c8b55467d7a
Justin Jones <justinjones1500@yahoo.com>
2013-08-29 19:26:33 -0400

    Add micropost partial and to show page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -53,15 +53,21 @@ describe "User pages" do
   end #end index
 
   describe "profile page" do
-  let(:user) { FactoryGirl.create(:user) }
-  before { visit user_path(user) }
+   let(:user) { FactoryGirl.create(:user) }
+   let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+   let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+   before { visit user_path(user) }
 
-  it { should have_content(user.name) }
-  #it { should have_title(user.name) } #this does not work, use below
-  it { should have_selector('title', text: full_title(user.name)) }
-
+   it { should have_content(user.name) }
+   #it { should have_title(user.name) } #this does not work, use below
+   it { should have_selector('title', text: full_title(user.name)) }
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
   
-  end
+  end# end profile page
 
   describe "signup page" do
     before { visit signup_path }
