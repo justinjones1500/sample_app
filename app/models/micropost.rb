9633c320b55b4026eec0faa159926b39354cdaf4
Justin Jones <justinjones1500@yahoo.com>
2013-08-29 19:01:24 -0400

    Add Micropost ordering and destruction from database on user deletion from 10.1.4

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,6 @@
 class Micropost < ActiveRecord::Base
   attr_accessible :content, :user_id
   belongs_to :user
+  default_scope -> { order('created_at DESC') }
   validates :user_id, presence: true
 end
