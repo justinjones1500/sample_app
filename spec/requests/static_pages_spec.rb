4efba89b4cd2cfa985de30add2bb5846bb3b4376
Justin Jones <justinjones1500@yahoo.com>
2013-09-23 10:41:22 -0400

    Add follow buttons and stats to show page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -24,7 +24,19 @@ describe "Static pages" do
         user.feed.each do |item|
           expect(page).to have_selector("li##{item.id}", text: item.content)
         end
-      end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end ##end describe following counts
+
+      end## end the feed
     end #end signed in user
   end #end home page
 
