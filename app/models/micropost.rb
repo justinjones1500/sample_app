612ed9ff35f8f9e4facc91fb570b156b7b0c53c5
Justin Jones <justinjones1500@yahoo.com>
2013-09-23 15:35:10 -0400

    Add in SQL queries and complete sample app

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,11 +2,14 @@ class Micropost < ActiveRecord::Base
   attr_accessible :content, :user_id
   belongs_to :user
   default_scope -> { order('created_at DESC') }
-  validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validates :user_id, presence: true
 
+  # Returns microposts from the users being followed by the given user.
   def self.from_users_followed_by(user)
-    followed_user_ids = user.followed_user_ids
-    where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
+    followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
+          user_id: user.id)
   end
 end
